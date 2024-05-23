da4fbe7c14f776894348d663a673a04665698cf5
Erick Alvarez <erick505alvarez@gmail.com>
2024-05-22 20:05:52 -0700

    add play and pause logic

--- main.js
+++ main.js
@@ -14,3 +14,37 @@
 // incorporate a visualizer (maybe use some AI)?
 
 // maybe use spotify
+
+// console.log(Audio);
+// console.log(chrome);
+
+// let myAudio = new Audio(chrome.runtime.getURL("./assets/songs/ocean-surf.mp3"));
+
+const button = document.querySelector(".btn-random");
+
+// create cache
+const cache = ["./assets/songs/ocean-surf.mp3", "./assets/songs/meditation.mp3", "./assets/songs/mountain-streams.mp3"]
+
+let switched = false
+let audio = null
+
+button.addEventListener('click', () => {
+    // if currently on
+        // then stop
+    if (!switched){
+        let i = Math.floor(Math.random()*3);
+        console.log(cache[i])
+        audio = new Audio(`${cache[i]}`)
+        audio.play();
+        switched = true
+    } else {
+        audio.pause();
+        audio = null;
+        // update our switched boolean
+        switched = false
+    }
+})
+
+// randomVlaue * number of discrete CSSMathValue
+
+// .01 * Array.length
