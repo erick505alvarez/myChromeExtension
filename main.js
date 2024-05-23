8983292b6cb339a0dd418165322b08a9eeabf02b
Erick Alvarez <erick505alvarez@gmail.com>
2024-05-22 18:58:47 -0700

    finish css

---
+++ main.js
@@ -0,0 +1,16 @@
+//Open extension -> button for random ambient sound
+//ambient sound cache 
+
+
+// html file will have a button on it to randomize a song selection
+
+// select a song
+
+// instruct the browser to begin playing sound
+//<audio>
+//<source> attribute set to name of mp3
+//add controls within the opening <audio> tag 
+
+// incorporate a visualizer (maybe use some AI)?
+
+// maybe use spotify
